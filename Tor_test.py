a6a8f350be00ae8133d84caebe16c4fcfeaa7f32
SHemarati <hasan.emarati.12652@gmail.com>
2023-01-25 23:39:34 +0330

    test request

--- Tor_test.py
+++ Tor_test.py
@@ -1,7 +1,36 @@
-from requests_tor import RequestsTor
+import requests
 
-rt = RequestsTor(tor_ports=(9000, 9001, 9002, 9003, 9004), autochange_id=1)
+r = requests.get('https://api.github.com/events')
+print(r.text)
 
-urls = (f'https://habr.com/ru/post/{x}' for x in range(1, 51))
-r = rt.get_urls(urls)
-print(r[-1].text)
+
+
+
+
+
+
+
+"""
+import requests
+
+r = requests.get('https://api.github.com/user', auth=('user', 'pass'))
+r.status_code
+200
+r.headers['content-type']
+'application/json; charset=utf8'
+r.encoding
+'utf-8'
+r.text
+'{"type":"User"...'
+r.json()
+
+
+
+////////////////////////////////
+import requests
+
+payload = {'key1': 'value1', 'key2': ['value2', 'value3']}
+
+r = requests.get('https://httpbin.org/get', params=payload)
+print('[url]: {%s} : %s' % (r.url , r))
+"""
